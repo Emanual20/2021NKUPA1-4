5bec98e1a341677c462307752e6611080563bab6
Emanual20 <784797673@qq.com>
2021-03-09 03:14:17 -0800

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-44-generic #50~20.04.1-Ubuntu SMP Wed Feb 10 21:07:30 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:14:17 up  6:11,  1 user,  load average: 0.13, 0.08, 0.02 c899c0964c4489b9138c62ff7c3c3dfd7ab22145

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -247,7 +247,7 @@ static uint32_t eval(int l, int r, bool *success){
   else{
     // printf("%d\n",*success);
     int opIndex = find_operator(l,r,success);
-    // printf("opIndex:%d\n",opIndex);
+    printf("opIndex:%d\n",opIndex);
     uint32_t val1 = 0;
     if(tokens[opIndex].type==TK_NOT){
       val1=eval(l,opIndex-1,success);

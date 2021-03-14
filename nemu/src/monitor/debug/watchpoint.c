60a1ec19dc52be8c84abc537b5adb85b1fe4ee34
Emanual20 <784797673@qq.com>
2021-03-14 05:25:39 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-44-generic #50~20.04.1-Ubuntu SMP Wed Feb 10 21:07:30 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  05:25:39 up  3:16,  1 user,  load average: 0.04, 0.08, 0.07 615ab23c514713d479b11b732a628785df304f45

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -12,6 +12,7 @@ void init_wp_pool() {
   	wp_pool[i].value = 0;
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
+    printf("%d\n", wp_pool[i].NO);
   }
   wp_pool[NR_WP - 1].next = NULL;
 

bb719dea1fe91c42cb0bafda64386bf410ee0498
Emanual20 <784797673@qq.com>
2021-04-14 19:51:38 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:51:38 up 11:20,  1 user,  load average: 0.10, 0.16, 0.11 9cab83125b1de95a40b2a639bc0ed5ee590b5f9b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -65,8 +65,8 @@ static inline void rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
 
 static inline void rtl_sm(rtlreg_t* addr, int len, const rtlreg_t* src1) {
   printf("in save memory1\n");
-  printf("addr: %p\n,addr");
-  printf("*addr: %p\n, value");
+  printf("addr: %p\n",addr);
+  printf("value: %p\n", *addr);
   vaddr_write(*addr, len, *src1);
   printf("in save memory2\n");
 }
@@ -156,9 +156,9 @@ static inline void rtl_push(const rtlreg_t* src1) {
   printf("hello1\n");
   cpu.esp = cpu.esp - 4;
   printf("hello2\n");
-  printf("%p\n",cpu.esp);
-  printf("%p\n",&cpu.esp);
-  printf("%p\n",*(&cpu.esp));
+  // printf("%p\n",cpu.esp);
+  // printf("%p\n",&cpu.esp);
+  // printf("%p\n",*(&cpu.esp));
   rtl_sm(&cpu.esp,src1,4);
   printf("hello3\n");
 }

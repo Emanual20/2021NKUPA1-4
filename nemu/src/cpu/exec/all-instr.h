334c0b41cc90df8cd565d88d01490364b7941226
Emanual20 <784797673@qq.com>
2021-04-15 03:03:05 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:03:05 up 14:19,  1 user,  load average: 0.34, 0.16, 0.10 abbdd6adde40cf69ebfa50507da5ea92cb494ed4

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -9,9 +9,13 @@ make_EHelper(nemu_trap);
 
 // arith.c
 make_EHelper(sub);
+
+// logic.c
 make_EHelper(xor);
 
+// control.c
 make_EHelper(call);
 make_EHelper(jmp);
+make_EHelper(ret);
 
 make_EHelper(push);

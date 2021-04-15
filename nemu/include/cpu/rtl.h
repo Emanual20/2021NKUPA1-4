ef735f826ea86194c1b383330d57454632f58899
Emanual20 <784797673@qq.com>
2021-04-14 19:54:57 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:54:57 up 11:23,  1 user,  load average: 0.21, 0.14, 0.10 b0b5fe9ac52c7b0414d2cfe21cef70bea08fa46

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -64,13 +64,13 @@ static inline void rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
 }
 
 static inline void rtl_sm(rtlreg_t* addr, int len, const rtlreg_t* src1) {
-  printf("in save memory1\n");
-  printf("addr: %p\n",addr);
-  printf("addr's value: %p\n", *addr);
-  printf("src: %p\n",src1);
-  printf("src's value: %p\n", *src1);
+  // printf("in save memory1\n");
+  // printf("addr: %p\n",addr);
+  // printf("addr's value: %p\n", *addr);
+  // printf("src: %p\n",src1);
+  // printf("src's value: %p\n", *src1);
   vaddr_write(*addr, len, *src1);
-  printf("in save memory2\n");
+  // printf("in save memory2\n");
 }
 
 static inline void rtl_lr_b(rtlreg_t* dest, int r) {
@@ -155,14 +155,14 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   //return;
-  printf("hello1\n");
+  // printf("hello1\n");
   cpu.esp = cpu.esp - 4;
-  printf("hello2\n");
+  // printf("hello2\n");
   // printf("%p\n",cpu.esp);
   // printf("%p\n",&cpu.esp);
   // printf("%p\n",*(&cpu.esp));
-  rtl_sm(&cpu.esp,src1,4);
-  printf("hello3\n");
+  rtl_sm(&cpu.esp,4,src1);
+  // printf("hello3\n");
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {

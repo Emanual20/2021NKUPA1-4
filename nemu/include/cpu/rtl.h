140e4cd2a16ebec6b9091403f89f99d462cc0e4d
Emanual20 <784797673@qq.com>
2021-04-14 19:52:41 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:52:41 up 11:21,  1 user,  load average: 0.09, 0.14, 0.11 2bd6360123725353a67135f47223438dc5830eed

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -66,7 +66,9 @@ static inline void rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
 static inline void rtl_sm(rtlreg_t* addr, int len, const rtlreg_t* src1) {
   printf("in save memory1\n");
   printf("addr: %p\n",addr);
-  printf("value: %p\n", *addr);
+  printf("addr's value: %p\n", *addr);
+  printf("src: %p\n",src1);
+  printf("src's value: %p\n", *src1);
   vaddr_write(*addr, len, *src1);
   printf("in save memory2\n");
 }

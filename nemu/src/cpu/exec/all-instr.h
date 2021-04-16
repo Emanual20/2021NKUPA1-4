9b26253e6ff271865c565908bacd8e630b07af69
Emanual20 <784797673@qq.com>
2021-04-16 09:38:21 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:38:21 up 1 day,  1:48,  1 user,  load average: 0.12, 0.11, 0.03 7cd1984d5b936847cccf6d6e98ec1aed0e74252

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -9,6 +9,7 @@ make_EHelper(nemu_trap);
 
 // arith.c
 make_EHelper(sub);
+make_EHelper(dec);
 
 // logic.c
 make_EHelper(test);
--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -43,7 +43,17 @@ make_EHelper(inc) {
 }
 
 make_EHelper(dec) {
-  TODO();
+  rtl_subi(&t2, &id_dest->val, 1);
+  operand_write(id_dest, &t2);
+
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+  rtl_li(&t0, 1);
+  rtl_xor(&t0, &id_dest->val, &t0);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
 
   print_asm_template1(dec);
 }

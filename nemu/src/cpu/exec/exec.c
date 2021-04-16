34793e2a98a22004979990feb543f7b1e153a915
Emanual20 <784797673@qq.com>
2021-04-16 03:27:05 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:27:05 up 20:50,  1 user,  load average: 0.10, 0.11, 0.09 6255830471fd6f7d8eb70b8b7fa26cc4b9924d8f

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -237,15 +237,14 @@ void exec_wrapper(bool print_flag) {
 
 #ifdef DEBUG
   // printf("hello-debug1\n");
-  // printf("%d %d\n",strlen(decoding.asm_buf),strlen(decoding.assembly));
+  printf("%d %d\n",strlen(decoding.asm_buf),strlen(decoding.assembly));
   int instr_len = decoding.seq_eip - cpu.eip;
   // printf("hello-debug2: %d\n", instr_len);
   int tmp = 50 - (12 + 3 * (instr_len>0?instr_len:0));
   sprintf(decoding.p, "%*.s", tmp, "");
-  // printf("%d %d\n",strlen(decoding.asm_buf),strlen(decoding.assembly));
   // printf("hello-debug3\n");
   // printf("%s\n%s\n",decoding.asm_buf,decoding.assembly);
-  // printf("%d %d\n",strlen(decoding.asm_buf),strlen(decoding.assembly));
+  printf("%d %d\n",strlen(decoding.asm_buf),strlen(decoding.assembly));
   strcat(decoding.asm_buf, decoding.assembly);
   // printf("hello-debug4\n");
   Log_write("%s\n", decoding.asm_buf);

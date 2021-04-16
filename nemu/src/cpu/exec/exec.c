b36b592e56a9cc7ea8fe2d890cae1d11ad1704e6
Emanual20 <784797673@qq.com>
2021-04-16 02:48:25 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:48:25 up 20:11,  1 user,  load average: 0.03, 0.07, 0.08 73cb404cbc89e28760e843dde9c77623a6a071fb

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -240,7 +240,7 @@ void exec_wrapper(bool print_flag) {
   printf("%d %d\n",strlen(decoding.asm_buf),strlen(decoding.assembly));
   int instr_len = decoding.seq_eip - cpu.eip;
   printf("hello-debug2: %d\n", instr_len);
-  sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
+  sprintf(decoding.p, "%*.s", min(0,50 - (12 + 3 * instr_len)), "");
   printf("%d %d\n",strlen(decoding.asm_buf),strlen(decoding.assembly));
   printf("hello-debug3\n");
   printf("%s\n%s\n",decoding.asm_buf,decoding.assembly);

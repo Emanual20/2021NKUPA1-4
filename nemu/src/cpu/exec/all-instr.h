7613afcd5f1ea8ce18ddb1af3ca123db379d9aab
Emanual20 <784797673@qq.com>
2021-04-16 21:24:26 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:24:26 up 1 day,  5:33,  1 user,  load average: 0.29, 0.73, 0.47 639b5212eea8043208fb07aeedf73b0aa81482f

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -39,6 +39,7 @@ make_EHelper(jmp);
 make_EHelper(jcc);
 make_EHelper(jmp_rm);
 make_EHelper(ret);
+make_EHelper(call_rm);
 
 // data-mov.c
 make_EHelper(mov);
--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -39,7 +39,8 @@ make_EHelper(ret) {
 }
 
 make_EHelper(call_rm) {
-  TODO();
+  rtl_push(eip);
+  rtl_jr(&id_dest->val);
 
   print_asm("call *%s", id_dest->str);
 }

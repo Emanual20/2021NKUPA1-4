2629c02c7a349f614c161d35293f7ee4e972053f
Emanual20 <784797673@qq.com>
2021-04-16 20:46:09 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-48-generic #54~20.04.1-Ubuntu SMP Sat Mar 20 13:40:25 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:46:09 up 1 day,  4:55,  1 user,  load average: 0.09, 0.09, 0.09 d4c94378ae372ac3d6c8587ebea6e6b77f29f982

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,7 +38,9 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  op->simm = instr_fetch(eip,op->width);
+  uint32_t imm = instr_fetch(eip,op->width);
+  rtl_sext(&imm, &imm, op->width);
+  op->simm = (int32_t)imm;
 
   rtl_li(&op->val, op->simm);
 

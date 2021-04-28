e0c17baddb72730fa4d6c8391ef0c64f66a1db55
Emanual20 <784797673@qq.com>
2021-04-28 01:02:44 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:02:44 up  6:10,  1 user,  load average: 0.54, 0.23, 0.13 27284f6b2d01a4756fdc56cac288cbb8f20b4cda

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,13 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern size_t get_ramdisk_size();
+extern void ramdisk_read(void *, off_t, size_t);
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  // TODO();
+  size_t size = get_ramdisk_size();
+  void * buff = NULL;
+  ramdisk_read(buff,0,size); 
   return (uintptr_t)DEFAULT_ENTRY;
 }

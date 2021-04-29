e2685a4b130d698a3291476831fe5d9c0584b761
Emanual20 <784797673@qq.com>
2021-04-28 21:35:07 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:35:07 up 11:46,  1 user,  load average: 0.62, 0.68, 0.61 6cfb52f87f00551c374f5db33cdccbcd7273a23

--- nexus-am/apps/microbench/include/benchmark.h
+++ nexus-am/apps/microbench/include/benchmark.h
@@ -51,7 +51,7 @@ extern "C" {
 #define   MD5_SM {     100,   1 KB,     0, 0xf902f28f}
 #define   MD5_LG {10000000,  16 MB, 19593, 0x27286a42}
 
-
+/*
 #define BENCHMARK_LIST(def) \
   def(qsort, "qsort", QSORT_SM, QSORT_LG, "Quick sort") \
   def(queen, "queen", QUEEN_SM, QUEEN_LG, "Queen placement") \
@@ -63,13 +63,21 @@ extern "C" {
   def( lzip,  "lzip",  LZIP_SM,  LZIP_LG, "Lzip compression") \
   def(ssort, "ssort", SSORT_SM, SSORT_LG, "Suffix sort") \
   def(  md5,   "md5",   MD5_SM,   MD5_LG, "MD5 digest") \
+*/
 
 
-/*
 // used for debug
 #define BENCHMARK_LIST(def) \
-  def( 15pz,  "15pz",  PZ15_SM,  PZ15_LG, "A* 15-puzzle search") \
-*/
+  def(qsort, "qsort", QSORT_SM, QSORT_LG, "Quick sort") \
+  def(queen, "queen", QUEEN_SM, QUEEN_LG, "Queen placement") \
+  def(   bf,    "bf",    BF_SM,    BF_LG, "Brainf**k interpreter") \
+  def(  fib,   "fib",   FIB_SM,   FIB_LG, "Fibonacci number") \
+  def(sieve, "sieve", SIEVE_SM, SIEVE_LG, "Eratosthenes sieve") \
+  def(dinic, "dinic", DINIC_SM, DINIC_LG, "Dinic's maxflow algorithm") \
+  def( lzip,  "lzip",  LZIP_SM,  LZIP_LG, "Lzip compression") \
+  def(ssort, "ssort", SSORT_SM, SSORT_LG, "Suffix sort") \
+  def(  md5,   "md5",   MD5_SM,   MD5_LG, "MD5 digest") \
+
 
 // Each benchmark will run REPEAT times
 

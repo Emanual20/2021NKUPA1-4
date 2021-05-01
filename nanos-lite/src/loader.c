d4a380ef5c51aed1460a558a1c3e87a8fb20ebfb
Emanual20 <784797673@qq.com>
2021-04-30 23:22:31 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:22:31 up 10 min,  1 user,  load average: 0.21, 0.33, 0.27 54c5a3be63c803d1a12bdb11dba4f346ccdb7a4f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,7 +7,7 @@ extern void ramdisk_read(void *, off_t, size_t);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  int size = get_ramdisk_size();
+  size_t size = get_ramdisk_size();
   printf("size:%d\n",size);
   ramdisk_read((void *)DEFAULT_ENTRY, 0, size); 
   return (uintptr_t)DEFAULT_ENTRY;

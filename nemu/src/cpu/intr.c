8a5f310a7432ffeeaad02e057fde6a8630729d75
Emanual20 <784797673@qq.com>
2021-05-01 08:52:02 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  08:52:02 up  4:46,  1 user,  load average: 0.05, 0.46, 0.35 26d903db1ee04532a270e9088e68b75ad8ca9677

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,7 +7,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
   rtl_li(&t0, vaddr_read(cpu.idtr.base+8*NO,2));
   rtl_li(&t1, vaddr_read(cpu.idtr.base+8*NO+4, 4));
-  if(!(t2 & 0x8000)){
+  if(!(t1 & 0x8000)){
     printf("Gate Descriptor %d is invalid..\n", NO);
     return;
   }

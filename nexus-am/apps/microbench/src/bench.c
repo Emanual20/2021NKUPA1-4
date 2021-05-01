3d92fb2c9ab5bef049898d4a3bcfdc3c88e12a18
Emanual20 <784797673@qq.com>
2021-04-30 19:48:02 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:48:02 up 13:03,  1 user,  load average: 0.06, 0.20, 1.14 3622e447a3751a031420a39d4a19a203f36e40a

--- nexus-am/apps/microbench/src/bench.c
+++ nexus-am/apps/microbench/src/bench.c
@@ -145,15 +145,14 @@ int32_t bench_rand() {
 
 // FNV hash
 uint32_t checksum(void *start, void *end) {
-  const int32_t x = 16777619;
-  int32_t hash = 2166136261u;
+  const uint32_t x = 16777619;
+  uint32_t h1 = 2166136261u;
   for (uint8_t *p = (uint8_t*)start; p + 4 < (uint8_t*)end; p += 4) {
-    int32_t h1 = hash;
     for (int i = 0; i < 4; i ++) {
       h1 = (h1 ^ p[i]) * x;
     }
-    hash = h1;
   }
+  int32_t hash = (uint32_t)h1;
   hash += hash << 13;
   hash ^= hash >> 7;
   hash += hash << 3;

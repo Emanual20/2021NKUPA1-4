ab9a03d23c986e4e4538883eccfe43c0f8fbd4c7
Emanual20 <784797673@qq.com>
2021-05-01 20:06:15 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:06:15 up  6:22,  1 user,  load average: 0.07, 0.09, 0.09 dec601757e2eb1a76c447aaa256373e1e4700e5

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,10 +20,10 @@ _RegSet* do_syscall(_RegSet *r) {
       return sys_none(r);
       break;
     }
-    case SYS_exit:{
-      return sys_exit(r);
-      break;
-    }
+    // case SYS_exit:{
+    //   return sys_exit(r);
+    //   break;
+    // }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 

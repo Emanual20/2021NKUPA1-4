d66a2cdac7184f15d7b077ec58b1c80b1a2c1a25
Emanual20 <784797673@qq.com>
2021-05-15 02:43:58 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:43:58 up 1 day, 10:22,  1 user,  load average: 0.53, 0.18, 0.09 eac18954ea4a8ccee5909ef4373152df679d80de

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,12 @@
 #include "common.h"
 #include "syscall.h"
 
+int fs_open(const char *pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
+
 _RegSet* sys_none(_RegSet *r){
   SYSCALL_ARG1(r) = 1;
   return NULL;
@@ -11,12 +17,42 @@ _RegSet* sys_write(_RegSet *r){
   char* buf = (char*) SYSCALL_ARG3(r);
   int len = SYSCALL_ARG4(r);
 
-  if(fd == 1 || fd == 2){
-    for(int i = 0; i < len; i++){
-      _putc(buf[i]);
-    }
-    SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
-  }
+  // if(fd == 1 || fd == 2){
+  //   for(int i = 0; i < len; i++){
+  //     _putc(buf[i]);
+  //   }
+  //   SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
+  // }
+  // return NULL;
+
+  SYSCALL_ARG1(r) = fs_write(fd, buf, len);
+  return NULL;
+}
+
+static inline _RegSet* sys_open(_RegSet *r) {
+  const char* pathname = (const char*)SYSCALL_ARG2(r);
+  int flags = (int)SYSCALL_ARG3(r);
+  int mode = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_open(pathname,flags,mode);
+  return NULL;
+}
+static inline _RegSet* sys_read(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int len = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_read(fd,buf,len);
+  return NULL;
+}
+static inline _RegSet* sys_close(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  SYSCALL_ARG1(r) = fs_close(fd);
+  return NULL;
+}
+static inline _RegSet* sys_lseek(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  off_t offset = (off_t)SYSCALL_ARG3(r);
+  int whence = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_lseek(fd,offset,whence);
   return NULL;
 }
 
@@ -40,12 +76,28 @@ _RegSet* do_syscall(_RegSet *r) {
       return sys_none(r);
       break;
     }
+    case SYS_open:{
+      return sys_open(r);
+      break;
+    }
+    case SYS_read:{
+      return sys_read(r);
+      break;
+    }
     case SYS_write:{
       return sys_write(r);
       break;
     }
     case SYS_exit:{
       return sys_exit(r);
+      break;
+    }
+    case SYS_close:{
+      return sys_close(r);
+      break;
+    }
+    case SYS_lseek:{
+      return sys_lseek(r);
       break;
     }
     case SYS_brk:{

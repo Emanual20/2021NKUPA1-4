2c0d14f1033c0e3cf4a5b8f1adace86eb36a12d0
Emanual20 <784797673@qq.com>
2021-05-15 07:45:33 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:45:33 up 1 day, 15:24,  1 user,  load average: 0.13, 0.10, 0.11 7ade4f3769d9e8821380d97aaae9ad5d7f09c95

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,7 +23,7 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 void fb_write(const void *buf, off_t offset, size_t len) {
   int r = (offset/4) / _screen.width;
   int c = (offset/4) % _screen.width;
-  _draw_rect(buf, c, r, len/4, 1);
+  _draw_rect((uint32_t*)buf, c, r, len/4, 1);
 }
 
 void init_device() {

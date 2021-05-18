6af029c5a04509331da837fd89db6a326b118db1
Emanual20 <784797673@qq.com>
2021-05-18 07:51:58 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:51:58 up 2 days, 11:20,  1 user,  load average: 0.11, 0.10, 0.13 1c333f1d597f853a408488512c519756221c8b64

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/pal");
+  uint32_t entry = loader(NULL, "/dev/events");
   Log("finish loader in main..");
   ((void (*)(void))entry)();
 

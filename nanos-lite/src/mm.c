86a70812220b44efb80a742e5979007c9ccfa19e
Emanual20 <784797673@qq.com>
2021-05-29 03:26:53 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:26:53 up 6 days, 15:31,  1 user,  load average: 0.06, 0.09, 0.09 6a6a37011603b9fc42b145ee956b7098746791b3

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,6 +16,24 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
+  if(current->cur_brk == 0){
+    current->cur_brk = current -> max_brk = new_brk;
+  }
+  else{
+    if(new_brk > current -> max_brk){
+      // TODO: map memory region [current->max_brk, new_brk)
+      // into address space current->as
+      uintptr_t vaddr = (((uint32_t)(current->max_brk) + 0xfff) & 0xfffff000);
+      while(vaddr < new_brk){
+        _map(&current->as, (void*)vaddr, (void*)new_page());
+        vaddr += PGSIZE;
+      }
+      
+      current->max_brk = new_brk;
+    }
+
+    current->max_brk = new_brk;
+  }
   return 0;
 }
 

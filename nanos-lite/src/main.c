3fc72d591d7ea25f4551b6ec2b0eab7fb5e166ca
Emanual20 <784797673@qq.com>
2021-05-29 03:51:52 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:51:52 up 6 days, 15:56,  1 user,  load average: 0.22, 0.15, 0.10 8389b1f413593793327ed1e61e9c3d1b276646e6

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -35,7 +35,7 @@ int main() {
   // uint32_t entry = loader(NULL, "/bin/pal");
   // Log("finish loader in main..");
   // ((void (*)(void))entry)();
-  load_prog("/bin/dummy");
+  load_prog("/bin/pal");
 
   panic("Should not reach here");
 }

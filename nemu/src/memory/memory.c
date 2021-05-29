e7256a33af478ff83ab5d49f723d35a96395190a
Emanual20 <784797673@qq.com>
2021-05-29 00:52:22 -0700

    >  compile 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:52:22 up 6 days, 12:57,  1 user,  load average: 0.23, 0.22, 0.09 7789c91b722f2ca0706ee3d0d46442ab620fe34b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -29,10 +29,52 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
     mmio_write(addr, len, data, map_NO);
 }
 
-uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+#define PDX(va) (((uint32_t)(va) >> 22) & 0x03ff)
+#define PTX(va) (((uint32_t)(va) >> 12) & 0x03ff)
+#define OFFSET(va) (((uint32_t)(va) >> 0) & 0x03ff)
+#define REMOVE_OFFSET(pte) ((uint32_t)(pte) & 0xfffffc00) 
+
+bool IfDataCrossBoundary(vaddr_t addr, int len){
+  return ((addr + len - 1) & ~PAGE_MASK) != (addr & ~ PAGE_MASK);
 }
 
-void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+paddr_t page_translate(vaddr_t vaddr, bool flag){
+  PDE pde;
+  PDE *pgdir;
+  PTE pte;
+  PTE *ptdir;
+  if(cpu.cr0.protect_enable && cpu.cr0.paging){
+    pgdir = (PDE*)(REMOVE_OFFSET(cpu.cr3.val));
+    pde.val = paddr_read((paddr_t)&pgdir[PDX(vaddr)], 4);
+    assert(pde.present);
+    pde.accessed = true;
+
+    ptdir = (PTE*)(REMOVE_OFFSET(pde.val));
+    pte.val = paddr_read((paddr_t)&ptdir[PTX(vaddr)], 4);
+    assert(pte.present);
+    pte.accessed = true;
+    pte.dirty = flag ? 1 : pte.dirty;
+
+    return REMOVE_OFFSET(pte.val) | OFFSET(vaddr);
+  }
+}
+
+uint32_t vaddr_read(vaddr_t vaddr, int len) {
+  if(IfDataCrossBoundary(vaddr, len)){
+    assert(0);
+  }
+  else{
+    paddr_t paddr = page_translate(vaddr, false);
+    return paddr_read(vaddr, len);
+  }
+}
+
+void vaddr_write(vaddr_t vaddr, int len, uint32_t data) {
+  if(IfDataCrossBoundary(vaddr, len)){
+    assert(0);
+  }
+  else{
+    paddr_t paddr = page_translate(vaddr, true);
+    paddr_write(paddr, len, data);
+  }
 }

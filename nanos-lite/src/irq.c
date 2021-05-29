a9323abfa63a973aebe52b4921a1ce69e8fcf67b
Emanual20 <784797673@qq.com>
2021-05-29 07:03:51 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:03:51 up 6 days, 19:08,  1 user,  load average: 0.18, 0.15, 0.11 d49eea453dc7affc46ef82466a29a8ba3ec2c8c

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,11 +6,11 @@ _RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:{
-      do_syscall(r);
+      return do_syscall(r);
       break;
     }
     case _EVENT_TRAP:{
-      schedule(r);
+      return schedule(r);
       break;
     }
     default: panic("Unhandled event ID = %d", e.event);

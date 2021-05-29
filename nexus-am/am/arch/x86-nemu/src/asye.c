acc98a61f355f278e9d946f3ea15ea994bcfa005
Emanual20 <784797673@qq.com>
2021-05-29 06:49:05 -0700

    >  run 1811425 rexxar Linux ubuntu 5.8.0-50-generic #56~20.04.1-Ubuntu SMP Mon Apr 12 21:46:35 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:49:05 up 6 days, 18:54,  1 user,  load average: 0.20, 0.14, 0.16 da5cb8c9ec45867a8fcaa0ae529b5b43e4358930

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -5,6 +5,7 @@ static _RegSet* (*H)(_Event, _RegSet*) = NULL;
 
 void vecsys();
 void vecnull();
+void vectrap();
 
 _RegSet* irq_handle(_RegSet *tf) {
   _RegSet *next = tf;
@@ -13,8 +14,8 @@ _RegSet* irq_handle(_RegSet *tf) {
     switch (tf->irq) {
       case 0x80: ev.event = _EVENT_SYSCALL; break;
       case 0x81: ev.event = _EVENT_TRAP; break;
-      default: ev.event = tf->irq; break;
-      //default: ev.event = _EVENT_ERROR; break;
+      //default: ev.event = tf->irq; break;
+      default: ev.event = _EVENT_ERROR; break;
     }
 
     next = H(ev, tf);
@@ -36,6 +37,7 @@ void _asye_init(_RegSet*(*h)(_Event, _RegSet*)) {
 
   // -------------------- system call --------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_USER);
+  idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_USER);
 
   set_idt(idt, sizeof(idt));
 
